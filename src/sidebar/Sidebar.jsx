554f8e3655611eee6daf13381c17668ca32f7323
asad-amjad <asad.amjad.pk@gmail.com>
2022-03-11 23:25:48 +0500

    introduce[Sidebar]

--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -1,4 +1,85 @@
-function Sidebar() {
-  return <>Sidebar</>;
+import React, { useState } from "react";
+import Grid from "@mui/material/Grid";
+import List from "@mui/material/List";
+import Avatar from "@mui/material/Avatar";
+import Divider from "@mui/material/Divider";
+import Collapse from "@mui/material/Collapse";
+import ListItem from "@mui/material/ListItem";
+import Typography from "@mui/material/Typography";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import { menu, bottomMenu } from "./menu";
+import { hasChildren } from "../shared/utilities";
+
+export default function Sidebar() {
+  return (
+    <Grid contain>
+      <Grid item>
+        <Grid container>
+          <Grid item p={4}>
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+            <Typography mt={2} variant="h5">
+              Admin One
+            </Typography>
+            <Typography variant="p">Centralin User</Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item>
+        {menu.map((item, key) => (
+          <MenuItem key={key} item={item} />
+        ))}
+        {bottomMenu.map((item, key) => (
+          <MenuItem key={key} item={item} />
+        ))}
+      </Grid>
+    </Grid>
+  );
 }
-export default Sidebar;
+
+const MenuItem = ({ item }) => {
+  const Component = hasChildren(item) ? MultiLevel : SingleLevel;
+  return <Component item={item} />;
+};
+
+const SingleLevel = ({ item }) => {
+  return (
+    <>
+      <ListItem button>
+        <ListItemIcon>{item.icon}</ListItemIcon>
+        <ListItemText primary={item.title} />
+      </ListItem>
+      {item.border && (
+        <Divider style={{ background: "#dddddd" }} variant="middle" />
+      )}
+    </>
+  );
+};
+
+const MultiLevel = ({ item }) => {
+  const { items: children } = item;
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen((prev) => !prev);
+  };
+
+  return (
+    <React.Fragment>
+      <ListItem button onClick={handleClick}>
+        <ListItemIcon>{item.icon}</ListItemIcon>
+        <ListItemText primary={item.title} />
+        {open ? <ExpandLessIcon /> : <ChevronRightIcon />}
+      </ListItem>
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          {children.map((child, key) => (
+            <MenuItem key={key} item={child} />
+          ))}
+        </List>
+      </Collapse>
+    </React.Fragment>
+  );
+};
---
+++ src/sidebar/menu.js
@@ -0,0 +1,63 @@
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import ArticleIcon from '@mui/icons-material/Article';
+import GroupIcon from '@mui/icons-material/Group';
+import PersonIcon from '@mui/icons-material/Person';
+import AssessmentIcon from '@mui/icons-material/Assessment';
+import SupportAgentIcon from '@mui/icons-material/SupportAgent';
+import FeedbackIcon from '@mui/icons-material/Feedback';
+import React from "react";
+
+export const menu = [
+    {
+        icon: <ArticleIcon />,
+        title: "Downloads",
+        items: [{
+            title: "Download",
+            items: []
+        }]
+    },
+    {
+        icon: <GroupIcon />,
+        title: "Shared links",
+        items: []
+    },
+    {
+        icon: <PersonIcon />,
+        title: "Personal links",
+        items: []
+    },
+
+    {
+        icon: <AssessmentIcon />,
+        title: "Admin groups",
+        items: [
+            {
+                title: "Group-1",
+                items: []
+            }
+        ]
+    },
+    {
+        icon: <HomeOutlinedIcon />,
+        title: "Smart Alert",
+    },
+    {
+        icon: <HomeOutlinedIcon />,
+        title: "Trending Diary",
+    },
+    {
+        icon: <HomeOutlinedIcon />,
+        title: "Co-Devlopment",
+        border: true
+    },
+];
+export const bottomMenu = [
+    {
+        icon: <SupportAgentIcon />,
+        title: "Support"
+    },
+    {
+        icon: <FeedbackIcon />,
+        title: "Feedback"
+    }
+];
